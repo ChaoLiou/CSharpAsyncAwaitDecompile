a19c6a8e2376fe1676e798a93790f103aaaef1ec
ChaoLiou <ucnsaythtagn@gmail.com>
2018-07-18 16:38:23 +0800

    void h() {var action = new Action(async () => { });}

--- Decompiled.cs
+++ Decompiled.cs
@@ -1,20 +1,20 @@
-    .method public hidebysig instance void g () cil managed 
+    .method public hidebysig instance void h () cil managed 
     {
         .locals init (
             [0] class [mscorlib]System.Action action
         )
 
         IL_0000: nop
-        IL_0001: ldsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__6_0'
+        IL_0001: ldsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__7_0'
         IL_0006: dup
         IL_0007: brtrue.s IL_0020
 
         IL_0009: pop
         IL_000a: ldsfld class AsyncAwait.Class1/'<>c' AsyncAwait.Class1/'<>c'::'<>9'
-        IL_000f: ldftn instance void AsyncAwait.Class1/'<>c'::'<g>b__6_0'()
+        IL_000f: ldftn instance void AsyncAwait.Class1/'<>c'::'<h>b__7_0'()
         IL_0015: newobj instance void [mscorlib]System.Action::.ctor(object,  native int)
         IL_001a: dup
-        IL_001b: stsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__6_0'
+        IL_001b: stsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__7_0'
 
         IL_0020: stloc.0
         IL_0021: ret
