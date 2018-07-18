9d86cbcc6a7151ee0dfb3aecae95f7a6810ba036
ChaoLiou <ucnsaythtagn@gmail.com>
2018-07-18 16:37:21 +0800

    void g() { var action = new Action(() => { }); }

--- Decompiled.cs
+++ Decompiled.cs
@@ -1,36 +1,21 @@
-    .method public hidebysig instance class [mscorlib]System.Threading.Tasks.Task`1<int32> f () cil managed 
+    .method public hidebysig instance void g () cil managed 
     {
-        .custom instance void [mscorlib]System.Runtime.CompilerServices.AsyncStateMachineAttribute::.ctor(class [mscorlib]System.Type) = (
-            01 00 19 41 73 79 6e 63 41 77 61 69 74 2e 43 6c
-            61 73 73 31 2b 3c 66 3e 64 5f 5f 35 00 00
-        )
-        .custom instance void [mscorlib]System.Diagnostics.DebuggerStepThroughAttribute::.ctor() = (
-            01 00 00 00
-        )
         .locals init (
-            [0] class AsyncAwait.Class1/'<f>d__5' V_0,
-            [1] valuetype [mscorlib]System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1<int32> V_1
+            [0] class [mscorlib]System.Action action
         )
 
-        IL_0000: newobj instance void AsyncAwait.Class1/'<f>d__5'::.ctor()
-        IL_0005: stloc.0
-        IL_0006: ldloc.0
-        IL_0007: ldarg.0
-        IL_0008: stfld class AsyncAwait.Class1 AsyncAwait.Class1/'<f>d__5'::'<>4__this'
-        IL_000d: ldloc.0
-        IL_000e: call valuetype [mscorlib]System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1<int32> valuetype [mscorlib]System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1<int32>::Create()
-        IL_0013: stfld valuetype [mscorlib]System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1<int32> AsyncAwait.Class1/'<f>d__5'::'<>t__builder'
-        IL_0018: ldloc.0
-        IL_0019: ldc.i4.m1
-        IL_001a: stfld int32 AsyncAwait.Class1/'<f>d__5'::'<>1__state'
-        IL_001f: ldloc.0
-        IL_0020: ldfld valuetype [mscorlib]System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1<int32> AsyncAwait.Class1/'<f>d__5'::'<>t__builder'
-        IL_0025: stloc.1
-        IL_0026: ldloca.s V_1
-        IL_0028: ldloca.s V_0
-        IL_002a: call instance void valuetype [mscorlib]System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1<int32>::Start<class AsyncAwait.Class1/'<f>d__5'>(!!0&)
-        IL_002f: ldloc.0
-        IL_0030: ldflda valuetype [mscorlib]System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1<int32> AsyncAwait.Class1/'<f>d__5'::'<>t__builder'
-        IL_0035: call instance class [mscorlib]System.Threading.Tasks.Task`1<int32> valuetype [mscorlib]System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1<int32>::get_Task()
-        IL_003a: ret
+        IL_0000: nop
+        IL_0001: ldsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__6_0'
+        IL_0006: dup
+        IL_0007: brtrue.s IL_0020
+
+        IL_0009: pop
+        IL_000a: ldsfld class AsyncAwait.Class1/'<>c' AsyncAwait.Class1/'<>c'::'<>9'
+        IL_000f: ldftn instance void AsyncAwait.Class1/'<>c'::'<g>b__6_0'()
+        IL_0015: newobj instance void [mscorlib]System.Action::.ctor(object,  native int)
+        IL_001a: dup
+        IL_001b: stsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__6_0'
+
+        IL_0020: stloc.0
+        IL_0021: ret
     }
