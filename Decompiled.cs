5435f54114a7b69a3a8218a747c5cc4ee37d3012
ChaoLiou <ucnsaythtagn@gmail.com>
2018-07-19 12:03:35 +0800

    var a = new Action(async () => await Task.Delay(1000)); a();

--- Decompiled.cs
+++ Decompiled.cs
@@ -1,20 +1,20 @@
-    .method public hidebysig instance void k () cil managed 
+    .method public hidebysig instance void l () cil managed 
     {
         .locals init (
             [0] class [mscorlib]System.Action action
         )
 
         IL_0000: nop
-        IL_0001: ldsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__10_0'
+        IL_0001: ldsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__11_0'
         IL_0006: dup
         IL_0007: brtrue.s IL_0020
 
         IL_0009: pop
         IL_000a: ldsfld class AsyncAwait.Class1/'<>c' AsyncAwait.Class1/'<>c'::'<>9'
-        IL_000f: ldftn instance void AsyncAwait.Class1/'<>c'::'<k>b__10_0'()
+        IL_000f: ldftn instance void AsyncAwait.Class1/'<>c'::'<l>b__11_0'()
         IL_0015: newobj instance void [mscorlib]System.Action::.ctor(object,  native int)
         IL_001a: dup
-        IL_001b: stsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__10_0'
+        IL_001b: stsfld class [mscorlib]System.Action AsyncAwait.Class1/'<>c'::'<>9__11_0'
 
         IL_0020: stloc.0
         IL_0021: ldloc.0
